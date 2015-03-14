1a6910078affb02e26a7c71e83504915e3967c21
Eric Price <eric2025@gmail.com>
2015-03-14 17:48:56 -0400

    scroll button

--- js/app.js
+++ js/app.js
@@ -9,32 +9,35 @@ $(function() {
     myButton.hide();
     
     myWindow.on('scroll', function() {
-        if ($(this).scrollTop() > 200) { //height from top to trigger slideDown
-            myButton.show();
-            myButton.addClass('textCenter');
+        if (window.matchMedia('(max-width: 768px)').matches) {
+            if ($(this).scrollTop() > 400) { //height from top to trigger slideDown
+                myButton.show();
+            }
+            else {
+                myButton.hide();
+            }
         }
     });
-    myWindow.on('scroll', function() {
-        if ($(this).scrollTop() < 50) { //trigger height to slide back up
-            myButton.hide();
-        }
-    });
+    myButton.click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
 });
 
 //Smooth Scroll using #scrollTop tag
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+//$(function() {
+//  $('a[href*=#]:not([href=#])').click(function() {
+//    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//      var target = $(this.hash);
+//      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//      if (target.length) {
+//        $('html,body').animate({
+//          scrollTop: target.offset().top
+//        }, 1000);
+//        return false;
+//      }
+//    }
+//  });
+//});
 
